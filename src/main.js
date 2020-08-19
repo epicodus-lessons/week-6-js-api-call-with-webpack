af7899f524388ee8eac716ba98ebe4d4990de6fc
Franz Knupfer <franzknupfer@hotmail.com>
2020-08-19 11:14:21 -0700

    api call with async/await

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import WeatherService from './weather-service.js'
+import WeatherService from './weather-service.js';
 
 function clearFields() {
   $('#location').val("");
@@ -16,17 +16,19 @@ function getElements(response) {
     $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
     $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
   } else {
-    $('.showErrors').text(`There was an error: ${response.message}`);
+    $('.showErrors').text(`There was an error: ${response}`);
   }
+}
+
+async function makeApiCall(city) {
+  const response = await WeatherService.getWeather(city);
+  getElements(response);
 }
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     let city = $('#location').val();
     clearFields();
-    WeatherService.getWeather(city)
-      .then(function(response) {
-        getElements(response);
-      });
+    makeApiCall(city);
   });
 });
--- src/weather-service.js
+++ src/weather-service.js
@@ -1,14 +1,13 @@
 export default class WeatherService {  
-  static getWeather(city) {
-    return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`)
-      .then(function(response) {
-        if (!response.ok) {
-          throw Error(response.statusText);
-        }
-        return response.json();
-      })
-      .catch(function(error) {
-        return error;
-      })
+  static async getWeather(city) {
+    try {
+      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch(error) {
+      return error.message;
+    }
   }
 }
