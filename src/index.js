e7f201698685012878d1e34ad002120695e890c5
Brooke Kullberg <brookekullberg@gmail.com>
2021-08-19 17:33:54 -0700

    update to use separated UI and Biz logic

--- src/index.js
+++ src/index.js
@@ -3,24 +3,28 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
+function getWeather(city) {
+  return new Promise(function(resolve, reject) {
+    let request = new XMLHttpRequest();
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+    request.onload = function() {
+      if (this.status === 200) {
+        resolve(request.response);
+      } else {
+        reject(request.response);
+      }
+    }
+    request.open("GET", url, true);
+    request.send();
+  });
+}
+
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     let city = $('#location').val();
     $('#location').val("");
 
-    let promise = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(request.response);
-        }
-      }
-      request.open("GET", url, true);
-      request.send();
-    });
+    let promise = getWeather(city);
 
     promise.then(function(response) {
       const body = JSON.parse(response);
